28747d49836740a5a193e23f8e602ca1910f049d
Zeegaths <zarahgathoni76@gmail.com>
2023-07-31 10:46:10 +0300

    fibonacci

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci_sequence(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        result = fibonacci_sequence(n - 1) + fibonacci_sequence(n - 2)
+        return result
