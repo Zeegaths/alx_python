be37dcdf674a901552e86a16073f441293b1f90c
Zeegaths <zarahgathoni76@gmail.com>
2023-07-31 11:02:05 +0300

    prime

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,6 +1,6 @@
 def is_prime(number):
     while number >= 2:
-        for i in range(2, int(number ** 0.5)):
+        for i in range(2, int(number)):
             if number % 1 == 0:
                 return False
     return True
