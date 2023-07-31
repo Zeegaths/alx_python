ed87ab87bfee2562f352bc29b93d9b6619e0e1a3
Zeegaths <zarahgathoni76@gmail.com>
2023-07-31 11:19:47 +0300

    password

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,9 +1,13 @@
 def validate_password(password):
+    # Check password length
     if len(password) < 8:
         return False
-    if char.isspace():
-            return False
-    
+
+    # Check for uppercase, lowercase, and digit in the password
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+
     for char in password:
         if char.isupper():
             has_uppercase = True
@@ -11,7 +15,10 @@ def validate_password(password):
             has_lowercase = True
         elif char.isdigit():
             has_digit = True
-        return True
-    else: 
-         return False
- 
+
+        # Check for spaces in the password
+        if char.isspace():
+            return False
+
+    # Check if all conditions are satisfied
+    return has_uppercase and has_lowercase and has_digit
