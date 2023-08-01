fa07a556e82747e16abb979f061abd6453b5bb36
Zeegaths <zarahgathoni76@gmail.com>
2023-08-02 01:18:05 +0300

    ends

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,6 +3,11 @@ arguments = []
 index = 0
 while arguments == 0:
     print ("." "\n")
-else: 
     index = index-1
-    print("{}: {}". format({index}, arguments))
+    if index == 1:
+         print("{} argument".format(index),end = "\n")
+         print("{}: {}". format({index}, arguments),end = "\n")
+else: 
+    print("{} arguments".format(index),end = "\n")
+    print("{}: {}". format({index}, arguments),end = "\n") 
+   
