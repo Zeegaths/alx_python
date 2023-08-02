776bcdd9c7b9de9f9f3f4423709ac2025ef8c2d7
Zeegaths <zarahgathoni76@gmail.com>
2023-08-02 17:41:12 +0300

    msg

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
 def exception_msg(message=""):
     try:
-        raise_exception_msg()
+        exception_msg()
     except NameError as ne:
         print(ne)
