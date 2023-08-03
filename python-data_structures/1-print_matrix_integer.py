a1b9f11847f61fa91cd1d07ca599840a0a09ed48
Zeegaths <zarahgathoni76@gmail.com>
2023-08-03 17:04:34 +0300

    base

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:  # Base case: check if the matrix is empty
+        return
     for row in matrix:
         for num in row:
             print("{:d}".format(num), end=" ")
-        print_matrix_integer()
-
+        print()
    
