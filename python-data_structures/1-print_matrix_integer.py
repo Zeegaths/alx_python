15a9e4d614286f0cc9a8125fd923a2c1bba0805f
Zeegaths <zarahgathoni76@gmail.com>
2023-08-03 17:01:17 +0300

    chat

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
-print_matrix_integer = __import__('1-print_matrix_integer').print_matrix_integer
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row: 
-            print("matrix = {}".format(num), end=" ")
-        print_matrix_integer
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
+
    
