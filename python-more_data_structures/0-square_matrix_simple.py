144191eb55a2978f57f7e8f752020b8912a3a4d5
Zeegaths <zarahgathoni76@gmail.com>
2023-08-06 08:55:46 +0300

    result matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,17 @@
-def square_matrix_simple(matrix=[]):    
-    for i in matrix:
-        return i * i
-matrix = []
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
+def square_matrix_simple(matrix=[]):
+    # Get the dimensions of the input matrix
+    rows = len(matrix)
+    if rows == 0:
+        return []
+
+    cols = len(matrix[0])
+
+    # Create a new matrix with the same size as the input matrix
+    result_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    # Fill the new matrix with the squared values of the input matrix
+    for i in range(rows):
+        for j in range(cols):
+            result_matrix[i][j] = matrix[i][j] ** 2
+
+    return result_matrix
