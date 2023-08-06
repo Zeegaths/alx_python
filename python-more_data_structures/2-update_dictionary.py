cb1aec7bda7c0396f9f7970c9611598a6802ece8
Zeegaths <zarahgathoni76@gmail.com>
2023-08-06 09:21:02 +0300

    return dict

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,6 +1,3 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    my_dict = {}
-    update_dictionary(my_dict, key, value)
-    update_dictionary(my_dict, key, value)
-    print(my_dict)
+    return a_dictionary
