a208b509c85246b40fac1d251156f2cbfc0d8dae
Zeegaths <zarahgathoni76@gmail.com>
2023-08-06 10:04:30 +0300

    best

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
         return None
     else:
         best_score = max(a_dictionary)
-print("Best score: {}".format(best_score))
+print("Best: {}".format(best_score))
     
