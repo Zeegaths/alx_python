97cbf95a4481d0c42bd9af7bb858ac3e4b5b46f6
Zeegaths <zarahgathoni76@gmail.com>
2023-08-06 08:41:56 +0300

    readme

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 def square_matrix_simple(matrix=[]):    
     for i in matrix:
         return i * i
-matrix = [0]
+matrix = []
 new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
 print(matrix)
