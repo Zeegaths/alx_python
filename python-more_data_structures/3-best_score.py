9a7e941f3a40c7b1b8f1d1bc30333e961c85d3cc
Zeegaths <zarahgathoni76@gmail.com>
2023-08-06 10:12:26 +0300

    best

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -3,10 +3,12 @@ def best_score(a_dictionary):
         return None
 
     max_score = None
-    for score in a_dictionary.values():
+    best_student = None
+
+    for student, score in a_dictionary.items():
         if max_score is None or score > max_score:
             max_score = score
+            best_student = student
 
-    return max_score
-
-    
+    return best_student
+    print("Best:", best_score(a_dictionary))
