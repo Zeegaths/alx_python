20f62b542465778316e3d00888c6aa85fa896c0c
Zeegaths <zarahgathoni76@gmail.com>
2023-08-08 15:19:39 +0300

    print

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,15 +3,13 @@ class Square:
     """ This is a square"""
     def __init__(self, size=0):
          self.__size = size
-         size = type(int)
          if size < 0:
             raise ValueError
-    print("size must be >= 0")      
-         
+         print("size must be >= 0")         
+         size = type(int)
     def sizetype(int):
          raise TypeError
     print("size must be an integer")
         
-    
             
 
