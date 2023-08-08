8481b96b8814f871eded3167dea9f6ecf056193f
Zeegaths <zarahgathoni76@gmail.com>
2023-08-08 15:34:28 +0300

    self size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,11 +3,12 @@ class Square:
     """ This is a square"""
     def __init__(self, size=0):
          self.__size = size
-         size = type(int)
+         self.__size = type(int)
          raise TypeError
     print("size must be an integer")
     def __init__(self, size=0):
-         if size < 0:
+         self.__size = size
+         if self.__size < 0:
               raise ValueError
          print("size must be >= 0")
     
