514bf2e49eb9b4422824e662373a76c71291e469
Zeegaths <zarahgathoni76@gmail.com>
2023-08-09 00:01:44 +0300

    RETURN

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,8 @@ class Square:
               raise ValueError("size must be >= 0")
          self.__size = size
 
-    def area(self, Square):
+    def area(self):
          area = self.__size ** 2
-         print(area)
+         return area
+        
          
