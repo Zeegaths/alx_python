dd61fee243cf5707982f1bd0d348f839a486eb3d
Zeegaths <zarahgathoni76@gmail.com>
2023-08-09 00:07:22 +0300

    SETTER

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+""" Class module"""
+class Square:
+    """ This is a class representing a square"""
+    def __init__(self, size=0):        
+         """Initializes the size"""
+         if type(size) is not int:
+              raise TypeError("size must be an integer")
+         elif size < 0:
+              raise ValueError("size must be >= 0")
+         self.__size = size
+
+    def area(self):
+         area = self.__size ** 2
+         return area
+        
+         
