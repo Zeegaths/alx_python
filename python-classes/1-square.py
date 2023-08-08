045f2baee8adf06eabfd5723b454c7de394bdbb7
Zeegaths <zarahgathoni76@gmail.com>
2023-08-08 15:04:32 +0300

    INT

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+class Square:
+    def __init__(self, size=0):
+         self.__size = size
+         size = type(int)
+    def type(int):
+         raise TypeError("size must be an integer")
+         if size < 0:
+             raise ValueError("size must be >= 0")
+         
+            
+
