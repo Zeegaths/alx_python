c8b35e04bbf92d86eec74a5fbf3aa512d4a2161c
Zeegaths <zarahgathoni76@gmail.com>
2023-08-08 15:21:36 +0300

    value error

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,13 +3,11 @@ class Square:
     """ This is a square"""
     def __init__(self, size=0):
          self.__size = size
-         if size < 0:
-            raise ValueError
-         print("size must be >= 0")         
          size = type(int)
     def sizetype(int):
-         raise TypeError
-    print("size must be an integer")
-        
+         raise TypeError("size must be an integer")
+         if __size < 0:
+             raise ValueError("size must be >= 0")
+         
             
 
