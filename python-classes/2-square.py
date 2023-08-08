e9c61c8712643ed5d8961bca7fadc665f9d45e6d
Zeegaths <zarahgathoni76@gmail.com>
2023-08-08 23:27:13 +0300

    setter

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,20 @@
-""" Class module"""
 class Square:
-    """ This is a class representing a square"""
-    def __init__(self, size=0):        
-         """Initializes the size"""
-         if type(size) is not int:
-              raise TypeError("size must be an integer")
-         elif size < 0:
-              raise ValueError("size must be >= 0")
-         self.__size = size
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-         return self.__ **2
-         
+        return self.__size ** 2
+
          
