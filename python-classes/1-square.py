27c6e72341e1a2de60d5c18048e7a653fc1672da
Zeegaths <zarahgathoni76@gmail.com>
2023-08-08 17:38:26 +0300

    integer

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,7 +4,7 @@ class Square:
     def __init__(self, size=0):        
          """Initializes the size"""
          if type(size) is not int:
-              raise TypeError("size must me an integer")
+              raise TypeError("size must be an integer")
          elif size < 0:
               raise ValueError("size must be >= 0")
          self.__size = size
