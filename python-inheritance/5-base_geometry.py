ba61608cec397300418c94764971d34273acb9a1
Zeegaths <zarahgathoni76@gmail.com>
2023-08-09 20:33:20 +0300

    errors

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,19 @@
+"""This is an empty class"""
+class metaGeometry(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+    """This is an empty class"""
+class BaseGeometry(metaclass=metaGeometry):
+    """This class is empty"""
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if value is not int:
+            raise TypeError("<name> must be an integer")
+        elif value <= 0:
+            raise ValueError("<name> must be greater than 0")
