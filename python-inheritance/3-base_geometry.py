ce9245f60f28d8c2d18105403f60f04549a08c68
Zeegaths <zarahgathoni76@gmail.com>
2023-08-09 19:40:41 +0300

    subclass

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,4 +2,4 @@
 class BaseGeometry:
     """This class is empty"""
     def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclas__']
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
