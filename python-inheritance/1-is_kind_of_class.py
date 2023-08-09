c47ef0c29bddbde3ff765dba5a28e94e0766826d
Zeegaths <zarahgathoni76@gmail.com>
2023-08-09 16:37:46 +0300

    type

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -3,7 +3,7 @@ def is_kind_of_class(obj, a_class):
     """check conditions"""
     if isinstance(obj, a_class):
         return True
-    elif issubclass(obj, a_class):
+    elif issubclass(type(obj), a_class):
         return True
     else:
         return False
