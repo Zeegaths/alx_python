ec189c050aae654ef76e335e49b417d85559512f
Zeegaths <zarahgathoni76@gmail.com>
2023-08-09 12:43:38 +0300

    setters

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,15 +2,23 @@
 class Square:
     """ This is a class representing a square"""
     def __init__(self, size=0):        
-         """Initializes the size"""
-         if type(size) is not int:
-              raise TypeError("size must be an integer")
-         elif size < 0:
-              raise ValueError("size must be >= 0")
-         self.__size = size
+         """Initializes the size"""         
+         self.size = size
 
-    def area(self):
-         area = self.__size ** 2
-         return area
+    @property
+    def size(self):
+         return self.__size
+
+    @size.setter
+    def size(self, value):
+         if type(value) is not int:
+              raise TypeError("size must be an integer")
+         elif value < 0:
+              raise ValueError("size must be >= 0")
+         
+    def getArea(self):
+         return self.__size ** 2
         
+    
+    
          
