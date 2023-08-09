37a27fe048efd7cc465b4d9c95fbd9bb3eb107bd
Zeegaths <zarahgathoni76@gmail.com>
2023-08-09 19:54:15 +0300

    Area

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,14 @@
+
+"""This is an empty class"""
+class metaGeometry(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+    """This is an empty class"""
+class BaseGeometry(metaclass=metaGeometry):
+    """This class is empty"""
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+    def area(self):
+        raise AttributeError("area() is not implemented")
