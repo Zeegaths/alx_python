e4f5e01cb20370458387eec333b66c99f4b20516
Zeegaths <zarahgathoni76@gmail.com>
2023-08-09 19:46:00 +0300

    meta

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,11 @@
+
 """This is an empty class"""
-class BaseGeometry:
+class metaGeometry(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+    """This is an empty class"""
+class BaseGeometry(metaclass=metaGeometry):
     """This class is empty"""
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
