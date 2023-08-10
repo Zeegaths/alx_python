062747a1017f026df7912efeaded277f0170ac2f
Zeegaths <zarahgathoni76@gmail.com>
2023-08-10 22:47:49 +0300

    setters

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+"""This is the base class"""
+class Base:
+    """Private class attribute"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id        
+        else:
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
+"""This is the rectangle class"""          
+class Rectangle(Base):
+    """This is the getter"""
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+    
+    @x.setter
+    def x(self,x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+    
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+         if id is not None:
+            self.id = id        
+         else:
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
+            self.__width = width
+            self.__height = height
+            self.__x = x
+            self.__y = y
