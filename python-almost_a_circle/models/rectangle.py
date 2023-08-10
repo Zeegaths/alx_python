055145fbb8ff5886b80fd4e7b5dfa3c6619d8edf
Zeegaths <zarahgathoni76@gmail.com>
2023-08-10 23:10:50 +0300

    validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,7 @@ class Base:
         else:
             self.__class__.__nb_objects += 1
             self.id = self.__class__.__nb_objects
+
 from models.base import Base
 """This is the rectangle class"""          
 class Rectangle(Base):
@@ -22,27 +23,44 @@ class Rectangle(Base):
     """This is the getter"""
     @property
     def width(self):
-        return self.__width
+        return self.__width    
     
     @width.setter
     def width(self, width):
+        if width is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
 
+    
+    
     @property
     def height(self):
         return self.__height
     
     @height.setter
     def height(self, height):
+        if height is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
+        
 
+  
     @property
     def x(self):
         return self.__x
     
     @x.setter
     def x(self,x):
+        if x is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
+        
 
     @property
     def y(self):
@@ -50,6 +68,10 @@ class Rectangle(Base):
     
     @y.setter
     def y(self, y):
+        if y is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
 
    
