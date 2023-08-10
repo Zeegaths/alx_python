7ea85385e61bfa016bc931a7e887b8b59d37fcc3
Zeegaths <zarahgathoni76@gmail.com>
2023-08-10 18:37:13 +0300

    ammend

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ class Base:
     __nb_objects = 0
     def __init__(self, id=None):
         if id is not None:
-            self.id = id
+            self.id = id        
         else:
+            __nb_objects = 0
             self.id += __nb_objects
