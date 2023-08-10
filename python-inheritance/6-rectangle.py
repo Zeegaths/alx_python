4ead2881238b5a7795d6561a1cbe9936dad04099
Zeegaths <zarahgathoni76@gmail.com>
2023-08-10 13:50:06 +0300

    innit

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,5 +1,10 @@
 """This is an empty class"""
-class BaseGeometry:
+class metaGeometry(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+    """This is an empty class"""
+class BaseGeometry(metaclass=metaGeometry):
     """This class is empty"""
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
@@ -12,14 +17,20 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-"""rectangle class""" 
+        
 class Rectangle(BaseGeometry):
-    """This is the rectangle class"""
+    """
+    Represents a rectangle
+    """
+
     def __init__(self, width, height):
+        """
+        Initializes the rectangle
+        """
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    def calculate_area(self):
-        return self.___width * self.__height
-
-        
 
