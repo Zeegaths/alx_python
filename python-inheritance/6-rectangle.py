7a943057d7a1024a71ced9ff4f9283b9cc05a9db
Zeegaths <zarahgathoni76@gmail.com>
2023-08-11 12:12:41 +0300

    new

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,28 +1,18 @@
-"""This is an empty class"""
-class metaGeometry(type):
-    def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-
-    """This is an empty class"""
-class BaseGeometry(metaclass=metaGeometry):
-    """This class is empty"""
-    def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    
+"""This is the base class"""
+class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):        
+    """Integer validator"""
+    def integer_validator(self, name, value):
+        """instances"""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        
+"""class rectangle"""
 class Rectangle(BaseGeometry):
-    """Rectangle subclass"""
     def __init__(self, width, height):
-        """calling the validator method"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
