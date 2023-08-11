c5b0d8e3b852f4b00482b6ab02f1b3b2d19d4d56
Zeegaths <zarahgathoni76@gmail.com>
2023-08-11 12:42:45 +0300

    docs

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,27 +1,78 @@
 """This is the metaclass"""
 class metaGeometry(type):
+    """
+    A custom metaclass to modify class attributes using __dir__.
+    """
     def __dir__(cls):
+        """
+        Modify the list of attributes to exclude __init_subclass__.
+        """
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-"""This is an empty class""" 
+
+"""This is an empty class"""
 class BaseGeometry(metaclass=metaGeometry):
-    """This class is empty"""
+    """
+    A base class that defines common geometric operations and validation methods.
+
+    Attributes:
+        None
+
+    Methods:
+        area(): Raises an exception since it's not implemented.
+        integer_validator(name, value): Validates that a value is a positive integer.
+    """
     def __dir__(cls):
+        """
+        Modify the list of attributes to exclude __init_subclass__.
+        """
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    """area"""    
+
     def area(self):
+        """
+        Raise an exception indicating that this method is not implemented.
+
+        Raises:
+            Exception: Always raised with the message "area() is not implemented".
+        """
         raise Exception("area() is not implemented")
-    """integer validator"""
-    def integer_validator(self, name, value):   
-        """checking instance"""     
+
+    def integer_validator(self, name, value):
+        """
+        Validate that a value is a positive integer.
+
+        Args:
+            name (str): The name of the attribute being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not greater than 0.
+        """
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-"""rectangle class"""     
+
+"""Rectangle class"""
 class Rectangle(BaseGeometry(metaclass=metaGeometry)):
-    """Rectangle subclass"""
+    """
+    A class representing a rectangle, inheriting from BaseGeometry.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+
+    Methods:
+        __init__(width, height): Initialize the Rectangle with width and height.
+    """
     def __init__(self, width, height):
-        """calling the validator method"""
+        """
+        Initialize the Rectangle with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
