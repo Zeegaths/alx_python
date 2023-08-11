e9336a1acc3629410ffe79de2342e0eadca54ee7
Zeegaths <zarahgathoni76@gmail.com>
2023-08-11 19:05:55 +0300

    no meta

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,10 +1,5 @@
-"""This is the metaclass"""
-class metaGeometry(type):
-    def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-
-    """This is an empty class""" 
-class BaseGeometry(metaclass=metaGeometry):
+"""This is an empty class""" 
+class BaseGeometry:
     """This class is empty"""
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
