7e392156c3939f7b9b7b4f1a9886a51cea94cba7
Zeegaths <zarahgathoni76@gmail.com>
2023-08-11 19:07:40 +0300

    subclass

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -14,6 +14,9 @@ class BaseGeometry:
             raise ValueError(f"{name} must be greater than 0")
         
 class Rectangle(BaseGeometry):
+    """remove suclass from output"""
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     """Rectangle subclass"""
     def __init__(self, width, height):
         """calling the validator method"""
