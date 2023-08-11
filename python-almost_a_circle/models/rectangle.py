9825071465ecdcaef8fe33874611da4be78a8a74
Zeegaths <zarahgathoni76@gmail.com>
2023-08-11 22:33:29 +0300

    formatted

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,8 +2,11 @@
 from models.base import Base
 
 """This is the rectangle class"""
+
+
 class Rectangle(Base):
     """calling the superclass and assigning attributes"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
@@ -15,23 +18,22 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
-        
-    
+
     """width setter"""
     @width.setter
     def width(self, value):
-        """validation"""   
+        """validation"""
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")     
+            raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-    
+
     """This is the height getter"""
     @property
     def height(self):
         return self.__height
-        
+
     """height setter"""
     @height.setter
     def height(self, value):
@@ -41,12 +43,12 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
-        
+
     """This is the x getter"""
     @property
     def x(self):
         return self.__x
-    
+
     """x setter"""
     @x.setter
     def x(self, value):
@@ -56,12 +58,12 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
-        
+
     """This is the y getter"""
     @property
     def y(self):
         return self.__y
-    
+
     """y setter"""
     @y.setter
     def y(self, value):
@@ -72,7 +74,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
     """area of the rectangle"""
+
     def area(self):
         """calculates the area of the rectangle"""
         return self.__width * self.__height
-
