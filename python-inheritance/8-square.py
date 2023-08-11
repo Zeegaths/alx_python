b1976edef358cea925f333243cda91521c8fc3f1
Zeegaths <zarahgathoni76@gmail.com>
2023-08-11 19:23:36 +0300

    square pep

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,64 +1,52 @@
-"""This is an empty class"""
-class metaGeometry(type):
+class MetaGeometry(type):
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
-    """This is an empty class"""
-class BaseGeometry(metaclass=metaGeometry):
-    """This class is empty"""
+
+class BaseGeometry(metaclass=MetaGeometry):
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    
+
     def area(self):
         raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):        
+
+    def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        
+
+
 class Rectangle(BaseGeometry):
-    """Rectangle subclass"""
     def __init__(self, width, height):
-        """calling the validator method"""
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
+
     def area(self):
-        """returns the area of the object"""
         return self.__width * self.__height
 
     def __str__(self):
-         """str method for printing"""
-         return "[Rectangle] {}/{}".format(self.__width, self.__height) 
-    def __dir__(cls):
-        """
-        This function removes the __init_subclass (method) from the default method inherited from the parent class
-        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
+    @classmethod
+    def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
+
 
 class Square(Rectangle):
-    """Square subclass"""
     def __init__(self, size):
-        """calling the validator method"""        
-        super().integer_validator("size", size)        
+        super().integer_validator("size", size)
         self.__size = size
-        Square = 0    
+        Square = 0
+
     def area(self):
-        """returns the area of the object"""
         return self.__size ** 2
-    
+
     def __str__(self):
-         """str method for printing"""
-         return Square.area 
+        return str(Square.area)
+
+    @classmethod
     def __dir__(cls):
-        """
-        This function removes the __init_subclass (method) from the default method inherited from the parent class
-        """
-
         return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
-
