ce76d85eafb65da2d2beb4a51181fe12a2af8d62
Zeegaths <zarahgathoni76@gmail.com>
2023-08-11 19:32:26 +0300

    formatted

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,9 +1,15 @@
-class MetaGeometry(type):
+"""This is an empty class"""
+
+
+class metaGeometry(type):
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
+    """This is an empty class"""
 
-class BaseGeometry(metaclass=MetaGeometry):
+
+class BaseGeometry(metaclass=metaGeometry):
+    """This class is empty"""
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
@@ -18,35 +24,51 @@ class BaseGeometry(metaclass=MetaGeometry):
 
 
 class Rectangle(BaseGeometry):
+    """Rectangle subclass"""
+
     def __init__(self, width, height):
+        """calling the validator method"""
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+        """returns the area of the object"""
         return self.__width * self.__height
 
     def __str__(self):
+        """str method for printing"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
-    @classmethod
     def __dir__(cls):
+        """
+        This function removes the __init_subclass (method) from the default method inherited from the parent class
+        """
+
         return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
 
 
 class Square(Rectangle):
+    """Square subclass"""
+
     def __init__(self, size):
+        """calling the validator method"""
         super().integer_validator("size", size)
         self.__size = size
         Square = 0
 
     def area(self):
+        """returns the area of the object"""
         return self.__size ** 2
 
     def __str__(self):
-        return str(Square.area)
+        """str method for printing"""
+        return Square.area
 
-    @classmethod
     def __dir__(cls):
+        """
+        This function removes the __init_subclass (method) from the default method inherited from the parent class
+        """
+
         return [attribute for attribute in super().__dir__() if attribute != "__init_subclass__"]
