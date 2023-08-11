cc92b9b5111ff858c2a418a9cb60a33eeb290d45
Zeegaths <zarahgathoni76@gmail.com>
2023-08-11 19:03:01 +0300

    self

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,6 +1,23 @@
-"""importing geometry base"""
-from 5-base_geometry import BaseGeometry
+"""This is the metaclass"""
+class metaGeometry(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
+    """This is an empty class""" 
+class BaseGeometry(metaclass=metaGeometry):
+    """This class is empty"""
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):        
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
 class Rectangle(BaseGeometry):
     """Rectangle subclass"""
     def __init__(self, width, height):
