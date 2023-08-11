ae653739aa288d73c4ca5d2047b7f2aa920a6421
Zeegaths <zarahgathoni76@gmail.com>
2023-08-11 23:41:52 +0300

    self id

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.__id, self.__x, self.__y, self.__width, self.__height)
 
                         
 
