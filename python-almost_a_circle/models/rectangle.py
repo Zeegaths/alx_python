55be45f86687b14c2fa32b72d0d7a371e5c9c387
Zeegaths <zarahgathoni76@gmail.com>
2023-08-13 00:20:33 +0300

    kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -99,28 +99,27 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         """These are the arguments
         """
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.width = args[1]
-            if len(args) >= 3:
-                self.height = args[2]
-            if len(args) >= 4:
-                self.x = args[3]
-            if len(args) >= 5:
-                self.y = args[4]
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
 
         for key, value in kwargs.items():
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "width" in kwargs:
-                self.id = kwargs["width"]
-            if "height" in kwargs:
-                self.id = kwargs["height"]
-            if "x" in kwargs:
-                self.id = kwargs["x"]
-            if "y" in kwargs:
-                self.id = kwargs["y"]
+            if key == "id":
+                self.id = value
+            if key == "width":
+                self.width = value
+            if key == "height":
+                self.height = value
+            if key == "x":
+                self.id = value
+            if key == "y":
+                self.id = value
 
         
