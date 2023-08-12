aa8c16ddf70c2d199242b37f9cfaa17987e51759
Zeegaths <zarahgathoni76@gmail.com>
2023-08-13 00:36:02 +0300

    docs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,6 +8,7 @@ class Square(Rectangle):
     """__init method calls the function"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """super method"""
         super().__init__(id)
         self.size = size
         self.x = x
