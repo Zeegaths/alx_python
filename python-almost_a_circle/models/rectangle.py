6ae8d49bc4157324f39c88b20a12f445a472c917
Zeegaths <zarahgathoni76@gmail.com>
2023-08-12 23:52:40 +0300

    docs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,6 +79,7 @@ class Rectangle(Base):
         """calculates the area of the rectangle"""
         return self.__width * self.__height
     """This is a public method"""
+
     def display(self):
         """It prints in stdout #"""
         for _ in range(self.__y):
@@ -89,11 +90,34 @@ class Rectangle(Base):
             for _ in range(self.__width):
                 print("#", end="")
             print()
+    """This is the str method"""
 
     def __str__(self):
         """"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args):
-                        
+    def update(self, *args, **kwargs):
+        """These are the arguments
+        """
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            if key == "width":
+                self.width = value
+            if key == "height":
+                self.height = value
+            if key == "x":
+                self.id = value
+            if key == "y":
+                self.id = value
 
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
