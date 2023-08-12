367e56d23487fb4ddfabc7a205c9aaf6c2f3581a
Zeegaths <zarahgathoni76@gmail.com>
2023-08-13 00:45:31 +0300

    docs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,14 +1,14 @@
 """This is the rectangle module import"""
 from rectangle import Rectangle
 
-"""This is the square class"""
+"""This is the square class inheriting from the rectangle"""
 
 
 class Square(Rectangle):
     """__init method calls the function"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """super method"""
+        """calling the super class using the logic of the Rectangle"""
         super().__init__(id)
         self.size = size
         self.x = x
@@ -16,5 +16,5 @@ class Square(Rectangle):
     """str method for printing"""
 
     def __str__(self):
-        """returns the square"""
+        """defines the format for returning the square attributes"""
         return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__size)
