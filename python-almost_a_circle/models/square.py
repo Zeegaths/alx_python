512a854cbe29cd1c2dcc40b3fdb30c2c0a80d28f
Zeegaths <zarahgathoni76@gmail.com>
2023-08-13 00:39:58 +0300

    docs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,20 +1,35 @@
-"""This is the rectangle module import"""
+# Import the Rectangle module
 from rectangle import Rectangle
 
-"""This is the square class"""
-
-
+# Define the Square class, which inherits from the Rectangle class
 class Square(Rectangle):
-    """__init method calls the function"""
+    """
+    This is the Square class, inheriting from the Rectangle class.
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """super method"""
-        super().__init__(id)
+        """
+        Initialize a Square object with the specified attributes.
+
+        :param size: The size of the square's sides.
+        :type size: int
+        :param x: The x-coordinate position of the square, defaults to 0.
+        :type x: int
+        :param y: The y-coordinate position of the square, defaults to 0.
+        :type y: int
+        :param id: The unique identifier of the square, defaults to None.
+        :type id: int or None
+        """
+        super().__init__(id)  # Call the parent class's constructor
         self.size = size
         self.x = x
         self.y = y
-    """str method for printing"""
 
     def __str__(self):
-        """returns the square"""
-        return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__size)
+        """
+        Return a string representation of the Square object.
+
+        :return: A formatted string describing the square.
+        :rtype: str
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.__x, self.__y, self.__size)
