23849cdd2dc46cb74f150629cfc2e72c46761cfe
Zeegaths <zarahgathoni76@gmail.com>
2023-08-12 23:50:46 +0300

    rectangle

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -29,31 +29,4 @@ class Square(Rectangle):
         return [attribute for attribute in super().__dir__()
                 if attribute != "__init_subclass__"]
     
-    def update(self, *args, **kwargs):
-        """These are the arguments
-        """
-        for key, value in kwargs.items():
-            if key == "id":
-                self.id = value
-            if key == "width":
-                self.width = value
-            if key == "height":
-                self.height = value
-            if key == "x":
-                self.id = value
-            if key == "y":
-                self.id = value
-
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
-
-
-
+    
