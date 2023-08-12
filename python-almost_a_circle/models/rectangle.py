7bdaa016fe83012e20309a2cb58b7c44cfa77921
Zeegaths <zarahgathoni76@gmail.com>
2023-08-13 00:21:23 +0300

    elif

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -113,13 +113,13 @@ class Rectangle(Base):
         for key, value in kwargs.items():
             if key == "id":
                 self.id = value
-            if key == "width":
+            elif key == "width":
                 self.width = value
-            if key == "height":
+            elif key == "height":
                 self.height = value
-            if key == "x":
+            elif key == "x":
                 self.id = value
-            if key == "y":
+            elif key == "y":
                 self.id = value
 
         
