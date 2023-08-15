a273bb57bad4b034b3dc8b8ea6a0777a5c5bfcad
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 22:27:16 +0300

    None

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -17,7 +17,7 @@ def main():
             else:
                 print("User ID not found in the response.")
         else:
-            print(f"Request failed with status code: {response.status_code}")
+            None
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
 
