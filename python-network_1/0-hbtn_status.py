6f1cd6906619d1029e3d2f55cdef4ec21f04badb
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 09:39:07 +0300

    docs

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,9 +5,11 @@ url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
 """Modifying the output"""
 if response.status_code == 200:
+    """Modifying the output"""
     data = response.json()
     print("Body response:")
     print("\t- type:", type(data))
     print("\t- content:", data)
 else:
-    print(f"Error: Unable to fetch data from {url}. Status code: {response.status_code}")
+    print(
+        f"Error: Unable to fetch data from {url}. Status code: {response.status_code}")
