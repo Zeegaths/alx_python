986825903b5dfdf2428c1509bf21829ceb31e27c
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 22:20:30 +0300

    frank

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,6 +1,31 @@
-"""This script return a different variable for each request"""
+"""import required modules"""
 import requests
 import sys
 
-r = requests.get("https://alu-intranet.hbtn.io/status")
-r.headers.get('X-Request-Id')
+# function carrying the main logic of the script
+def func_1():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+    
+    # assign the url as the first command line argument
+    url = sys.argv[1]
+    
+    try:
+        # send a GET request to the specified URL
+        response = requests.get(url)
+        # check if the request is successful
+        if response.status_code == 200:
+            x_request_id = response.headers.get('X-Request-Id')
+            if x_request_id:
+                print(f"X-Request-Id: {x_request_id}")
+            else:
+                print("X-Request-Id not contained in the response header.")
+        else:
+            print(f"Request failed: {response.status_code}")
+    # catch any exceptions that might arise
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    func_1()
