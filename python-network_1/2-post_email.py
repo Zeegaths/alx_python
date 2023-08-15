b2876757ddbe87774875ea0ae97fa8558b154135
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 18:40:19 +0300

    name

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,22 +3,17 @@ import requests
 import sys
 
 
-def main():
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    payload = {"email": email}
-
-    try:
-        response = requests.post(url, data=payload)
-        if response.status_code == 200:
-            print("Response Body:")
-            print(response.text)
-        else:
-            print(f"Request failed with status code: {response.status_code}")
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
-
-
 if __name__ == "__main__":
-    main()
+    url = sys.agrv[1]
+    email = sys.argv[2]
+    pyload = {
+        "email": email
+    }
+    response = requests.post(url, data=pyload)
+    try:
+        json = response.json()
+        if json:
+            get_email = json.get("form")["email"]
+            print("Your email is:", get_email)
+    except ValueError as e:
+        print(e)
