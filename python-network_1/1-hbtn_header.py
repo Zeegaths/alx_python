d1022c14f473e81e18169c3c39f57d856a85e000
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 21:03:27 +0300

    main

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,13 +2,5 @@
 import requests
 import sys
 
-def main():
-    url = sys.argv[1]
-    r = requests.get(url)
-    r2 = r.headers.get('X-Request-Id')
-    print("r2")
-
-if __name__ == "__main__":
-    main()
-
-
+r = requests.get("https://alu-intranet.hbtn.io/status")
+r.headers.get('X-Request-Id')
