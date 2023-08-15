483003f3c436c2d795710356377a3108c7825564
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 09:35:45 +0300

    if statement

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,5 +2,11 @@
 """
 import requests
 response = requests.get("https://alu-intranet.hbtn.io/status")
-response.text
-type(response.text)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+else:
+    print(f"Error: Unable to fetch data from {url}. Status code: {response.status_code}")
