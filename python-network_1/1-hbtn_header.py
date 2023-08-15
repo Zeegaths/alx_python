f92b61b7094b6b7adf4c75f9a274788aa450ab85
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 12:21:25 +0300

    print

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,4 +3,6 @@ import requests
 import sys
 
 r = requests.get("https://intranet.hbtn.io")
-print("r.headers{['X-Request-Id']}")
+response = r.json()
+for title in response['X-Request-Id']:
+    print(title['title'])
