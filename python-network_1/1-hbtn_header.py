ecb158653ab12d99a78c11f212655606581beb24
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 18:04:41 +0300

    url

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,23 +2,5 @@
 import requests
 import sys
 
-def main():
-    "receive and process the URL"
-    url = "https://alu-intranet.hbtn.io/status"  # Get the URL from user input
-
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            headers = response.headers
-            x_request_id = headers.get('X-Request-Id')
-            if x_request_id:
-                print(f"X-Request-Id: {x_request_id}")
-            else:
-                print("X-Request-Id header not found in the response headers.")
-        else:
-            print(f"Request failed with status code: {response.status_code}")
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    main()
+r = requests.get("https://intranet.hbtn.io")
+r.headers.get('X-Request-Id')
