c7a049cc817a6b82e10a8025d038e18a11533981
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 17:55:05 +0300

    chat

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,5 +2,15 @@
 import requests
 import sys
 
-r = requests.get("https://intranet.hbtn.io")
-r.headers.get('X-Request-Id')
+# You should specify the URL you're testing against
+url = "http://0.0.0.0:5050"  # Update this to the actual URL you want to test
+
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        x_request_id = response.headers.get('X-Request-Id')
+        print(f"X-Request-Id: {x_request_id}")
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+except requests.RequestException as e:
+    print(f"An error occurred: {e}")
