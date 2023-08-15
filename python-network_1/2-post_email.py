8b38dbbdf2f205477180bfd89dfc36d8f1519874
Zeegaths <zarahgathoni76@gmail.com>
2023-08-15 22:38:25 +0300

    email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,18 +1,17 @@
-"""This module sends a POST request with email as parameter"""
 import requests
 import sys
 
 def main():
     url = sys.argv[1]
     email = sys.argv[2]
+
     payload = {"email": email}
-    
+
     try:
-        response = requests.post(url, data=payload)  
+        response = requests.post(url, data=payload)
         if response.status_code == 200:
-            response_text = response.text
             print("Response Body:")
-            print(response_text)
+            print(response.text)
         else:
             print(f"Request failed with status code: {response.status_code}")
     except requests.RequestException as e:
@@ -20,8 +19,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
