7da6b76c5b35444e02426c563a5b0d3540b4833b
Zeegaths <zarahgathoni76@gmail.com>
2023-09-12 02:47:24 +0300

    filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check for the correct number of command-line arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <MySQL username> <MySQL password> <Database name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    try:
+        # Establish a connection to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+        )
+
+        # Create a cursor object
+        cur = db.cursor()
+
+        # Execute the SQL query to retrieve states starting with "N"
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+
+        # Fetch and display the results
+        results = cur.fetchall()
+        for row in results:
+            print(row)
+
+        # Close the cursor and the database connection
+        cur.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
