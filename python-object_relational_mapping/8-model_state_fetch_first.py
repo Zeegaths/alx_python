c8eb8aa562210b6e3d87833ca4f302279bdda5aa
Zeegaths <zarahgathoni76@gmail.com>
2023-09-15 13:05:53 +0300

    pep

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,7 +9,8 @@ from model_state import Base, State
 if __name__ == "__main__":
     # Check for the correct number of command-line arguments
     if len(sys.argv) != 4:
-        print("Usage: {} <MySQL username> <MySQL password> <Database name>".format(sys.argv[0]))
+        print("Usage: {} <MySQL username> <MySQL password>"
+              "<Database name> <State name>".format(sys.argv[0]))
         sys.exit(1)
 
     # Extract command-line arguments
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the first State object by sorting by id and getting the first result
+    # Query- sorting by id and getting the first result
     first_state = session.query(State).order_by(State.id).first()
 
     # Display the result
