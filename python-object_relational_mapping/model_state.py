fc5b7e0ded93387f83b9dc3935ba73407a48e16e
Zeegaths <zarahgathoni76@gmail.com>
2023-09-15 12:41:16 +0300

    2 blanks

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,12 +7,14 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class State(Base):
     """State class that represents the states table"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
