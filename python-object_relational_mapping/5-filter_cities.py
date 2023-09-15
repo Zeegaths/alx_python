e7c4fe77a8be41c12619465534d54a6a9ba648bf
Zeegaths <zarahgathoni76@gmail.com>
2023-09-15 12:29:20 +0300

    filter cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,3 +1,51 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
+
+if __name__ == "__main__":
+    # Check for the correct number of command-line arguments
+    if len(sys.argv) != 5:
+        print("Usage: {} <MySQL username> <MySQL password> <Database name> <State name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        # Establish a connection to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+        )
+
+        # Create a cursor object
+        cur = db.cursor()
+
+        # Execute the SQL query to retrieve cities of the specified state
+        query = """
+            SELECT cities.name
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id
+        """
+        cur.execute(query, (state_name,))
+
+        # Fetch and display the results
+        results = cur.fetchall()
+        city_names = [row[0] for row in results]
+        print(", ".join(city_names))
+
+        # Close the cursor and the database connection
+        cur.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
