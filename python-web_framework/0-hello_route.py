485beebff21e44e3f2d12cd1bca150c3fa1023a4
Zeegaths <zarahgathoni76@gmail.com>
2023-10-05 11:52:13 +0300

    documentation

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,11 +1,16 @@
-# The script starts a Flask web application
+# Import the Flask module
 from flask import Flask
 
+# Create a Flask web application instance
 app = Flask(__name__)
 
+# Define a route for the root URL '/' and disable strict slashes
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    # Return the string 'Hello HBNB!' as the response
     return 'Hello HBNB!'
 
+# Run the application if the script is executed
 if __name__ == '__main__':
+    # Start the Flask application on host '0.0.0.0' and port 5000
     app.run(host='0.0.0.0', port=5000)
