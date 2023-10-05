0c9715286978f66e01b265cb1e5a93455007d27a
Zeegaths <zarahgathoni76@gmail.com>
2023-10-05 12:25:24 +0300

    hbnb

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,27 @@
+"""This script starts a Flask web application.
+
+It creates a Flask web server that listens on 0.0.0.0 (all available network interfaces)
+and port 5000. The main route, '/', displays 'Hello HBNB!' when accessed in a web browser.
+
+Routes:
+    '/': Displays 'Hello HBNB!' when accessed.
+
+Attributes:
+    app (Flask): The Flask application instance.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route to display 'Hello HBNB!'."""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route to display 'HBNB'."""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
