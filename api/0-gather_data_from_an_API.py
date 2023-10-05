579b1a0bd800c1760fab7aa8f237793c6c050a51
Zeegaths <zarahgathoni76@gmail.com>
2023-10-05 11:15:33 +0300

    format

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,33 +1,30 @@
 import requests
 import sys
 
-def fetch_employee_info(employee_id):
+def get_employee_info(employee_id):
     try:
         # Fetch employee details
         employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
         employee_response = requests.get(employee_url)
         employee_data = employee_response.json()
-        
+        employee_name = employee_data['name']
+
         # Fetch employee's TODO list
         todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
         todos_response = requests.get(todos_url)
         todos_data = todos_response.json()
-        
-        # Count completed tasks
-        completed_tasks = [task for task in todos_data if task['completed']]
-        
-        # Display employee TODO list progress
-        employee_name = employee_data['name']
+
+        # Calculate TODO list progress
         total_tasks = len(todos_data)
-        completed_count = len(completed_tasks)
+        completed_tasks = sum(1 for task in todos_data if task['completed'])
+
+        # Display progress
+        print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
         
-        print(f"Employee {employee_name} is done with tasks ({completed_count}/{total_tasks}): ", end="")  # Adjust formatting here
-        
-        # Display titles of completed tasks
-        for i, task in enumerate(completed_tasks):
-            if i > 0:
-                print(", ", end="")  # Add a comma and space between task titles
-            print(task['title'], end="")  # Print task title without newline
+        # Display completed task titles
+        for task in todos_data:
+            if task['completed']:
+                print(f"    {task['title']}")
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
@@ -37,10 +34,10 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python 0-gather_data_from_an_API.py EMPLOYEE_ID")
         sys.exit(1)
-    
+
     try:
         employee_id = int(sys.argv[1])
-        fetch_employee_info(employee_id)
+        get_employee_info(employee_id)
     except ValueError:
         print("Employee ID must be an integer.")
         sys.exit(1)
