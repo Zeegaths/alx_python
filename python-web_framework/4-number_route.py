5f68059364b1afec123249fbcbb68de4bfce6915
Zeegaths <zarahgathoni76@gmail.com>
2023-10-05 12:50:34 +0300

    number

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -28,5 +28,16 @@ def c_route(text):
     """Route to display 'C ' followed by the value of the text variable (replace underscore _ symbols with a space)."""
     return 'C {}'.format(text.replace('_', ' '))
 
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_route(text='is_cool'):
+    """Route to display 'Python ' followed by the value of the text variable (replace underscore _ symbols with a space). Default text is 'is_cool'."""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """Route to display 'n is a number' only if n is an integer."""
+    return '{} is a number'.format(n)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
