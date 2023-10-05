895ef69320bc39671d7c481f35b850d8cd0f8d71
Zeegaths <zarahgathoni76@gmail.com>
2023-10-05 13:10:06 +0300

    render

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -13,6 +13,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def render_template_with_number(n):
+    if isinstance(n, int):
+        return render_template('5-number_template.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Route to display 'Hello HBNB!'."""
