006d7f8d03d52fbc39a7939fc3b1fb10e3e49a0a
Zeegaths <zarahgathoni76@gmail.com>
2023-10-05 12:37:06 +0300

    flask

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -18,5 +18,15 @@ def hello_hbnb():
     """Route to display 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route to display 'HBNB'."""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Route to display 'C ' followed by the value of the text variable (replace underscore _ symbols with a space)."""
+    return 'C {}'.format(text.replace('_', ' '))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
