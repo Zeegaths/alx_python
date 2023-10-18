28f694ad3781b79130de49338fc325d6e1d2b43a
Zeegaths <zarahgathoni76@gmail.com>
2023-10-18 12:27:49 +0300

    black

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,44 +1,37 @@
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    # Define the base URL for the API
-    base_url = "https://jsonplaceholder.typicode.com"
+def gather_data_from_an_API(employee_id):
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-    # Define the endpoint URLs for employee details and TODO items
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/users/{employee_id}/todos"
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
 
-    try:
-        # Fetch employee details
-        employee_response = requests.get(employee_url)
-        employee_data = employee_response.json()
-        employee_name = employee_data.get("name")
+    if user_response.status_code == 200 and todos_response.status_code == 200:
+        user_data = user_response.json()
+        todos_data = todos_response.json()
 
-        # Fetch TODO items for the employee
-        todo_response = requests.get(todo_url)
-        todo_data = todo_response.json()
+        employee_name = user_data["name"]
+        done_tasks = 0
+        completed_tasks_titles = []
 
-        # Calculate the number of completed tasks and total tasks
-        total_tasks = len(todo_data)
-        completed_tasks = sum(1 for todo in todo_data if todo["completed"])
-
-        # Display employee's TODO list progress
-        print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-
-        # Display titles of completed tasks
-        for todo in todo_data:
+        for todo in todos_data:
             if todo["completed"]:
-                print(f"    {todo['title']}")
+                done_tasks += 1
+                completed_tasks_titles.append(todo["title"])
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+        total_tasks = len(todos_data)
+
+        print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+        for title in completed_tasks_titles:
+            print(f"\t{title}")
+    else:
+        print(f"Error: Unable to retrieve data for employee with ID {employee_id}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    else:
+        employee_id = int(sys.argv[1])
+        gather_data_from_an_API(employee_id)
