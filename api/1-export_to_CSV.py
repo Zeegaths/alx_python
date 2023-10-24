a044c4fa745eea2c2101db26757a258592c7a279
Zeegaths <zarahgathoni76@gmail.com>
2023-10-24 11:18:54 +0300

    csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -11,7 +11,8 @@ import sys
 user_id = str(sys.argv[1])
 
 user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+    user_id)
 
 user_data = requests.get(user_url).json()
 todo_data = requests.get(todo_url).json()
@@ -19,6 +20,7 @@ todo_data = requests.get(todo_url).json()
 filename = "{}.csv".format(user_id)
 
 with open(filename, 'w', newline='') as file:
-    writter = csv.writer(file, quoting = csv.QUOTE_ALL)
+    writter = csv.writer(file, quoting=csv.QUOTE_ALL)
     for task in todo_data:
-        writter.writerow([user_id, str(user_data['username']),task['completed'], task['title']])
+        writter.writerow([user_id, str(user_data['username']),
+                         task['completed'], task['title']])
