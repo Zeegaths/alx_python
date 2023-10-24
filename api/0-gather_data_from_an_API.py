8b8dcc5c8ad7b08433e331784a6ec125408e237c
Zeegaths <zarahgathoni76@gmail.com>
2023-10-24 11:15:33 +0300

    gather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,38 +1,31 @@
+'''
+This module get data from todo api and write the data to a csv file
+'''
+
+import json
 import requests
 import sys
 
-def gather_data_from_an_API(employee_id):
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
+todo_url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
 
-    if user_response.status_code == 200 and todos_response.status_code == 200:
-        user_data = user_response.json()
-        todos_data = todos_response.json()
 
-        employee_name = user_data["name"]
-        done_tasks = 0
-        completed_tasks_titles = []
+req = requests.get(todo_url)
+results = json.loads(req.text)
 
-        for todo in todos_data:
-            if todo["completed"]:
-                done_tasks += 1
-                
-                completed_tasks_titles.append(todo["title"])
+task = 0
+completed_task_title = []
+user_name = ''
+for result in results:
+    if result['completed'] == True:
+        task = task + 1
+        completed_task_title.append(result['title'])
+    users = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(result['userId']))
+    user = json.loads(users.text)
+    user_name = user['name']
 
-        total_tasks = len(todos_data)
-        indent = '     '
-        print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
-        for title in completed_tasks_titles:
-            print(f"\{indent}{title}")
-    else:
-        print(f"Error: Unable to retrieve data for employee with ID {employee_id}")
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-    else:
-        employee_id = int(sys.argv[1])
-        gather_data_from_an_API(employee_id)
+print('Employee {} is done with tasks({}/{}):'.format(user_name, task, len(results)))
+for finished_task in completed_task_title:
+    print('\t {}'.format(finished_task))
