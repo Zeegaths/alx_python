aa6a57a8be9ec6c3281362b6d5a5dae6a642be6f
Zeegaths <zarahgathoni76@gmail.com>
2023-10-24 11:22:00 +0300

    chat

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,42 @@
-'''
-This module export content from an api into
-a csv file defined on creation
-    return : csv
-'''
-import csv
-import os
+import json
 import requests
 import sys
+import csv
 
-user_id = str(sys.argv[1])
+todo_url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
+req = requests.get(todo_url)
+results = json.loads(req.text)
 
-user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-    user_id)
+task = 0
+completed_task_title = []
+user_name = ''
 
-user_data = requests.get(user_url).json()
-todo_data = requests.get(todo_url).json()
+for result in results:
+    if result['completed'] == True:
+        task = task + 1
+        completed_task_title.append(result['title'])
+    users = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(result['userId']))
+    user = json.loads(users.text)
+    user_name = user['name']
 
-filename = "{}.csv".format(user_id)
+# Create and open the CSV file for writing
+user_id = sys.argv[1]
+csv_file_name = f'{user_id}.csv'
+with open(csv_file_name, mode='w', newline='') as csv_file:
+    fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-with open(filename, 'w', newline='') as file:
-    writter = csv.writer(file, quoting=csv.QUOTE_ALL)
-    for task in todo_data:
-        writter.writerow([user_id, str(user_data['username']),
-                         task['completed'], task['title']])
+    writer.writeheader()  # Write the header row to the CSV file
+
+    for finished_task in completed_task_title:
+        # Write the data for each completed task to the CSV file
+        writer.writerow({
+            'USER_ID': user_id,
+            'USERNAME': user_name,
+            'TASK_COMPLETED_STATUS': 'Completed',
+            'TASK_TITLE': finished_task
+        })
+
+print(f'Employee {user_name} is done with tasks ({task}/{len(results)}).')
+print(f'Task data has been exported to {csv_file_name}.')
