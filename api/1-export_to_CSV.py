04d60c03f62c0e738a0f5a1047e53e2d6aceb2cc
Zeegaths <zarahgathoni76@gmail.com>
2023-11-01 12:26:02 +0300

    csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,46 @@
-'''
-This module export content from an api into
-a csv file defined on creation
-    return : csv
-'''
 import csv
-import os
+import json
 import requests
 import sys
 
-user_id = str(sys.argv[1])
+# Function to write data to CSV
+def write_to_csv(user_id, user_name, tasks):
+    filename = f'{user_id}.csv'
+    with open(filename, 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in tasks:
+            csv_writer.writerow([user_id, user_name, task["completed"], task["title"]])
 
-user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-    user_id)
+if len(sys.argv) != 2:
+    print("Usage: python3 1-export_to_CSV.py <USER_ID>")
+    sys.exit(1)
 
-user_data = requests.get(user_url).json()
-todo_data = requests.get(todo_url).json()
+user_id = sys.argv[1]
+todo_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
 
-filename = "{}.csv".format(user_id)
+req = requests.get(todo_url)
+results = json.loads(req.text)
 
-with open(filename, 'w', newline='') as file:
-    writter = csv.writer(file, quoting=csv.QUOTE_ALL)
-    for task in todo_data:
-        writter.writerow([user_id, str(user_data['username']),
-                         task['completed'], task['title']])
+completed_task_title = []
+user_name = results[0]['userId']  # Initialize user_name with the first user's ID
+tasks = []
+
+for result in results:
+    if result['completed']:
+        completed_task_title.append(result['title'])
+
+    if result['userId'] != user_name:
+        # Write the tasks of the previous user to CSV
+        write_to_csv(user_name, user_name, tasks)
+        user_name = result['userId']
+        tasks = []
+
+    tasks.append(result)
+
+# Write the tasks of the last user to CSV
+write_to_csv(user_name, user_name, tasks)
+
+print(f'Employee {user_name} is done with tasks({len(completed_task_title)}/{len(results)}):')
+for finished_task in completed_task_title:
+    print('\t', finished_task)
